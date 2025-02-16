1a46a73bfa077bb302c6cfe3a6930ba327320ca0
Ta Khu Vu Khe Tu <voanhbendjndnd@gmail.com>
2025-02-16 17:13:28 +0700

    update

---
+++ src/main/java/vn/hoidanit/jobhunter/config/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,41 @@
+package vn.hoidanit.jobhunter.config;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import vn.hoidanit.jobhunter.domain.RestResponse;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final AuthenticationEntryPoint delegate = new BearerTokenAuthenticationEntryPoint();
+    private final ObjectMapper mapper;
+
+    public CustomAuthenticationEntryPoint(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+    // set error
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        this.delegate.commence(request, response, authException);
+        response.setContentType("application/json;charset=UTF-8");
+        RestResponse<Object> res = new RestResponse<>();
+        res.setStatusCode(HttpStatus.UNAUTHORIZED.value());
+        res.setError(authException.getCause().getMessage());
+        res.setMessage("Token không đúng!");
+        mapper.writeValue(response.getWriter(), res);
+        throw new UnsupportedOperationException("Unimplemented method 'commence'");
+    }
+
+}
